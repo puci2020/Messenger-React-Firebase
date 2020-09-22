1b66476ad9a4ba31e7bebc166bc321357aa1ea8a
puci2020 <adam.pucicki97@gmail.com>
2020-09-22 20:06:18 +0200

    Updates

--- precache-manifest.34357e9674e86b6d0b47befb0143f923.js
+++ precache-manifest.34357e9674e86b6d0b47befb0143f923.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0edd0a28438ffb792cad5b5e42b0e66e",
+    "revision": "fa7a18eb220c5e4e33c9937cefef72ca",
     "url": "/Messenger-React-Firebase/index.html"
   },
   {
-    "revision": "8afd07f6605dea3bb9ce",
+    "revision": "4f12a19acd74edeb7f51",
     "url": "/Messenger-React-Firebase/static/css/main.c1b8bb07.chunk.css"
   },
   {
-    "revision": "6b546970892d3681608e",
-    "url": "/Messenger-React-Firebase/static/js/2.8f98b161.chunk.js"
+    "revision": "2a6e3c35f0e663780e8a",
+    "url": "/Messenger-React-Firebase/static/js/2.fd27e93e.chunk.js"
   },
   {
     "revision": "5f8be6edcb631ea3c834b8459dc2c264",
-    "url": "/Messenger-React-Firebase/static/js/2.8f98b161.chunk.js.LICENSE.txt"
+    "url": "/Messenger-React-Firebase/static/js/2.fd27e93e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8afd07f6605dea3bb9ce",
-    "url": "/Messenger-React-Firebase/static/js/main.0e55a7ed.chunk.js"
+    "revision": "4f12a19acd74edeb7f51",
+    "url": "/Messenger-React-Firebase/static/js/main.f0a552fa.chunk.js"
   },
   {
     "revision": "de1dc6dc0bb7002ba8d6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Messenger-React-Firebase/precache-manifest.9ccc2144ddfffc5be519d6954254ce83.js"
+  "/Messenger-React-Firebase/precache-manifest.34357e9674e86b6d0b47befb0143f923.js"
 );
 
 self.addEventListener('message', (event) => {
